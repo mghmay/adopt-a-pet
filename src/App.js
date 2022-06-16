4950c62b49c4e2abf1af6354ca674d2c57085bf8
mghmay <mghmay@gmail.com>
2022-06-16 15:46:11 +0100

    finished app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,13 +11,13 @@ function App() {
       <div>
         <Navigation />
         <Switch>
-          <Route path='/:type/:id'>
+          <Route path="/:type/:id">
             <PetDetailsPage />
           </Route>
-          <Route path='/search'>
+          <Route path="/search">
             <SearchPage />
           </Route>
-          <Route path='/pet-details-not-found'>
+          <Route path="/pet-details-not-found">
             <PetDetailsNotFound />
           </Route>
           <Route path="/:type?">
